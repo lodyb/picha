83876427393351d1b6397527b47342dbb4325e34
xxami <xxami@gmail.com>
2014-08-10 11:25:47 +0100

    changed db and added its mysql workbench model

--- setup.sql
+++ setup.sql
@@ -1,14 +1,12 @@
 
-CREATE DATABASE IF NOT EXISTS `picha`
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+CREATE SCHEMA IF NOT EXISTS `picha` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `picha`.`comments` (
   `image_id` VARCHAR(32) NOT NULL,
-  `comment_x` INT NULL,
-  `comment_y` INT NULL,
-  `comment_w` INT NULL,
-  `comment_h` INT NULL,
+  `box_x` INT NULL,
+  `box_y` INT NULL,
+  `box_w` INT NULL,
+  `box_h` INT NULL,
   `comment` VARCHAR(255) NOT NULL,
   `date` DATETIME NOT NULL,
   `channel` VARCHAR(32) NULL)
