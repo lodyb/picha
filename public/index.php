5cba4bd61cb478bfa60a114cfb28fddd9639992b
lewis <lewis@evergreen.co>
2014-08-11 13:11:01 +0100

    Fixed some stylesheet issues

--- public/index.php
+++ public/index.php
@@ -14,9 +14,52 @@
 
 	<?php 
 	$a = array();
-	$mysqli = mysqli_connect("127.0.0.1", "root", "", "picha");
-	$q = mysqli_query($mysqli, "SELECT * FROM images");
-	while ($r = mysqli_fetch_assoc($q)) $a[] = $r;
+	$a[] = array(
+			'id' => 'test',
+			'like' => 52,
+			'dislike' => 21,
+		);
+
+	$a[] = array(
+			'id' => 'abcd',
+			'like' => 1,
+			'dislike' => 1,
+		);
+
+	$a[] = array(
+			'id' => 'qwert',
+			'like' => 100,
+			'dislike' => 0,
+		);
+
+	$a[] = array(
+			'id' => 'bzwse',
+			'like' => 0,
+			'dislike' => 100,
+		);
+
+	$a[] = array(
+			'id' => 'yetdf',
+			'like' => 2,
+			'dislike' => 42,
+		);
+
+	$a[] = array(
+			'id' => 'urdgfd',
+			'like' => 42,
+			'dislike' => 2,
+		);
+
+	$a[] = array(
+			'id' => 'efcstt',
+			'like' => 52,
+			'dislike' => 52,
+		);
+
+
+	// $mysqli = mysqli_connect("127.0.0.1", "root", "", "picha");
+	// $q = mysqli_query($mysqli, "SELECT * FROM images");
+	// while ($r = mysqli_fetch_assoc($q)) $a[] = $r;
 	// print("<pre>");
 	// print_r($a);exit;
 	?>
@@ -25,13 +68,13 @@
 		<div class="row">
 
 			<!-- the first one is always logo (or page name) -->
-			<div class="large-3 medium-6 small-12 columns end">
+			<div class="large-3 medium-4 small-6 columns end">
 				<div class="image page">
 					<span><?=($_REQUEST['id'] ? $_REQUEST['id'] : 'picha');?></span>
-					<form  enctype="multipart/form-data" action="upload.php<?=($_REQUEST['id']) ? '?chan=' . $_REQUEST['id'] : ''; ?>" method="post">
+<!-- 					<form  enctype="multipart/form-data" action="upload.php<?=($_REQUEST['id']) ? '?chan=' . $_REQUEST['id'] : ''; ?>" method="post">
 						<input type="file" name="file_upload">
 						<input type="submit" value="ul">
-					</form>
+					</form> -->
 				</div>
 			</div>
 
@@ -39,7 +82,7 @@
 
 			for ($i=0; $i < count($a); $i++) { 
 				?>
-				<div class="large-3 medium-6 small-12 columns end">
+				<div class="large-3 medium-4 small-6 columns end">
 					<div class="image">
 						<span><?php print_r($a[$i]); ?></span>
 					</div>
